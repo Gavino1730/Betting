97dca5b369783b3eb631df507b8bc570ed6ef2db
Gavino1730 <gavino1730@gmail.com>
2025-12-20 22:25:21 -0800

    Update admin password to 12345678

--- CREATE_ADMIN.sql
+++ CREATE_ADMIN.sql
@@ -1,16 +1,23 @@
--- Create admin user
--- Username: admin
--- Password: 12345
+-- METHOD 1: Register through the website with username "admin" and password "12345678"
+-- Then run this SQL to make that user an admin:
 
+UPDATE users 
+SET is_admin = true, balance = 10000
+WHERE username = 'admin';
+
+-- METHOD 2: If you already have a user, make them admin:
+-- UPDATE users SET is_admin = true, balance = 10000 WHERE username = 'yourusername';
+
+-- METHOD 3: Create admin directly (may have password hash issues)
+-- If this doesn't work, use METHOD 1 instead
 INSERT INTO users (username, password, balance, is_admin)
 VALUES (
   'admin',
-  '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy',
+  '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
   10000,
   true
 )
 ON CONFLICT (username) 
 DO UPDATE SET 
   is_admin = true,
-  balance = 10000,
-  password = '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy';
+  balance = 10000;
